b9ed31235887fa175783be00f6de60298f09c5c2
sosa7753 <sosa7753@naver.com>
2023-12-19 20:31:49 +0900

    feat(security) : SecurityConfiguration - Filter에서 접근을 제어하는 메서드 추가 - user-password 이전에 jwt 토큰 인증을 먼저 수행함.

---
+++ src/main/java/com/project/commerce/security/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package com.project.commerce.security;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(c -> c.disable())
+                .authorizeHttpRequests(
+                        authorize -> authorize
+                        .requestMatchers(
+                                "/**/signup", "/**/signin", "error"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                );
+        http.addFilterBefore(this.jwtAuthenticationFilter,
+                UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
