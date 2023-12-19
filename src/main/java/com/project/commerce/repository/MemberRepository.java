7aa1ab3fabbc11eddce2119bee1547b32ea3f860
sosa7753 <sosa7753@naver.com>
2023-12-19 20:15:30 +0900

    feat(repository) : MemberRepository - findByUsername 메서드 추가 - existsByUsername 메서드 추가

---
+++ src/main/java/com/project/commerce/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.project.commerce.repository;
+
+import com.project.commerce.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+}
