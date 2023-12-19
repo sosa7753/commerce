71726f11c41a29762633f02c581706c49e3612bd
sosa7753 <sosa7753@naver.com>
2023-12-19 20:12:38 +0900

    feat(service) : MemberService - 계정 등록 메서드 추가 - 로그인 메서드 추가 - UserDetailsService 메서드 구현

---
+++ src/main/java/com/project/commerce/service/MemberService.java
@@ -0,0 +1,64 @@
+package com.project.commerce.service;
+
+import com.project.commerce.domain.Member;
+import com.project.commerce.dto.AuthDto;
+import com.project.commerce.dto.AuthView;
+import com.project.commerce.exception.CommerceException;
+import com.project.commerce.repository.MemberRepository;
+import com.project.commerce.type.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Builder
+@Service
+@AllArgsConstructor
+public class MemberService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    // 계정 등록
+    @Transactional
+    public AuthView registerMember(AuthDto.SignUp request) {
+
+        boolean exists = this.memberRepository.existsByUsername(request.getUsername());
+
+        if(exists) {
+            throw new CommerceException(ErrorCode.ALREADY_USE_NAME);
+        }
+
+        request.setPassword(this.passwordEncoder.encode(request.getPassword()));
+        Member createMember = request.authToEntity();
+        this.memberRepository.save(createMember);
+
+        return new AuthView(createMember);
+    }
+
+
+    public Member logIn(AuthDto.SignIn request) {
+        Member member = this.memberRepository.findByUsername(request.getUsername())
+                .orElseThrow(() -> new CommerceException(ErrorCode.NOT_USER_NAME));
+
+        if(!this.passwordEncoder.matches(request.getPassword(), member.getPassword())) {
+            log.info("비밀번호 불일치");
+            throw new CommerceException(ErrorCode.NOT_EQUALS_PASSWORD);
+
+        }
+        return member;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.memberRepository.findByUsername(username)
+                .orElseThrow(() -> new CommerceException(ErrorCode.NOT_USER_NAME));
+    }
+}
