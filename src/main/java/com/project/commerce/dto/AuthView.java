60b9a82afa662e61c6a92ab05280a0e50dd703aa
sosa7753 <sosa7753@naver.com>
2023-12-26 18:02:56 +0900

    feat(dto) : role 부분 삭제

--- src/main/java/com/project/commerce/dto/AuthView.java
+++ src/main/java/com/project/commerce/dto/AuthView.java
@@ -1,27 +1,21 @@
 package com.project.commerce.dto;
 
 import com.project.commerce.domain.Member;
-import com.project.commerce.domain.Role;
 import com.project.commerce.type.UserType;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class AuthView {
     private String username;
     private UserType userType;
-    private List<String> roles;
 
     public AuthView(Member member) {
         this.username = member.getUsername();
         this.userType = member.getUserType();
-        this.roles = member.getRoles().stream()
-                .map(Role::getRole_name)
-                .collect(Collectors.toList());
     }
 
 }
