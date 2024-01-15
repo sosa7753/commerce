b1e44df45117e2bfd7cec994cfeccb22df7aa041
sosa7753 <sosa7753@naver.com>
2024-01-15 21:24:26 +0900

    feat(repository) : 장바구니 Repository

---
+++ src/main/java/com/project/commerce/repository/CartRepository.java
@@ -0,0 +1,13 @@
+package com.project.commerce.repository;
+
+import com.project.commerce.domain.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    Optional<Cart> findByCartName(String cartName);
+}
