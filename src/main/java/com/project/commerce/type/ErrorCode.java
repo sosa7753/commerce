3a759866798a6a977137058cac56030d97fcd537
sosa7753 <sosa7753@naver.com>
2024-01-15 21:14:52 +0900

    feat(type) : 상품관련 에러메세지 추가

--- src/main/java/com/project/commerce/type/ErrorCode.java
+++ src/main/java/com/project/commerce/type/ErrorCode.java
@@ -12,7 +12,18 @@ public enum ErrorCode {
     NOT_EXIST_OWNER_NAME("등록 되지 않은 소유주 입니다."),
     ALREADY_REGISTER_STORE_NAME("이미 등록된 매장명 입니다."),
     NOT_EXIST_STORE_NAME("존재 하지 않는 매장명 입니다."),
-    NOT_EXIST_STORE_BY_KEYWORD("조건에 맞는 매장이 없습니다.");
+    NOT_EXIST_STORE_BY_KEYWORD("조건에 맞는 매장이 없습니다."),
+
+    NOT_EXIST_PRODUCT_NAME("존재 하지 않는 상품명 입니다."),
+    ALREADY_REGISTER_PRODUCT_NAME("이미 등록된 상품명 입니다."),
+    NOT_EXIST_PRODUCT_BY_KEYWORD("조건에 맞는 상품이 없습니다."),
+
+    BAD_INPUT_OF_AMOUNT_OR_PRIZE("가격이나 수량 입력이 잘못되었습니다."),
+
+    PRODUCT_SOLD_OUT("현재 상품은 품절입니다."),
+    MUST_INPUT_AT_LEAST_ONE("수량을 한 개 이상 입력해야합니다."),
+    MORE_THAN_NOW_PRODUCT_AMOUNT("현재 수량보다 많이 담을 수 없습니다."),
+    NOT_IN_THE_CART("장바구니에 없는 상품 입니다.");
 
     private final String description;
 
