78e818bc29838f024fccbf142665e5b389fa990f
sosa7753 <sosa7753@naver.com>
2024-01-15 21:20:10 +0900

    feat(dto) : 장바구니 Dto

---
+++ src/main/java/com/project/commerce/dto/CartDto.java
@@ -0,0 +1,13 @@
+package com.project.commerce.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CartDto {
+
+    private String CartName;
+}
